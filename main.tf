be0a82a16a70a030722614eb8081851135121334
ABHIJIT SHANKAR PATIL <patilabhijit853@gmail.com>
2026-08-18 10:50:24 +0530

    Refactor EKS cluster and node group subnet configuration
    Updated EKS cluster and node group to use available subnets from data source instead of hardcoded subnet IDs.

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
-
+data "aws_subnets" "available-subnets"{
+    filter {
+        name = "tag:Name"
+        values = ["Our-Public-*"]
+    }
+}
 
 resource "aws_eks_cluster" "project-cluster" {
   name     = "project-cluster"
   role_arn = aws_iam_role.example.arn
+
   vpc_config {
-    subnet_ids = aws_subnet.our_vpc_public[*].id
+    subnet_ids = data.aws_subnets.available-subnets.ids
   }
+
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
@@ -26,7 +33,7 @@ resource "aws_eks_node_group" "node-grp" {
   cluster_name    = aws_eks_cluster.project-cluster.name
   node_group_name = "pc-node-group"
   node_role_arn   = aws_iam_role.worker.arn
-  subnet_ids      = aws_subnet.our_vpc_public[*].id
+  subnet_ids      = data.aws_subnets.available-subnets.ids
   capacity_type   = "ON_DEMAND"
   disk_size       = "20"
   instance_types  = ["c7i-flex.large"]
